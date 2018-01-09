f200e4a6400866547dae14e6c3064361d7e32fee
vasmarm <sharmavashisth@gmail.com>
2018-01-08 21:06:16 -0600

    html routes and api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,41 +4,117 @@
 
 // Dependencies
 // =============================================================
-
-// grab the orm from the config
-// (remember: connection.js -> orm.js -> route file)
-var orm = require("../config/orm.js");
+var Encounter = require("../models/encounter.js");
 
 // Routes
 // =============================================================
 module.exports = function(app) {
 
-  // GET route for getting all of the todos
-  app.get("/api/todos", function(req, res) {
-    // orm.getTodos(function(results) {
-    //   res.json(results);
-    // });
+  // Search for Encounters within a specific zipcode then provides JSON
+    //app.get("/api/:zipcode?", function(req, res) {
+
+    // Display the data for all of the ecounters.
+      //Encouter.findAll({
+        //where: {
+          //zipcode: req.params.zipcode,
+        //},
+        //order: ['encounter_date', 'DESC'],
+        //limit: 10
+      //})
+        //.then(function(result) {
+          //return res.json(result);
+        //});
+    //});
+
+  // Search for Encounters by city, state & country then provides JSON
+  app.get("/api/:city?:state?:country?", function(req,res) {
+
+    // Display data for all of the encounters.
+    Encounter.findAll({
+      where: {
+        city: req.params.city,
+        state: req.params.state,
+        country: req.params.country
+      },
+      order: ['encounter_date','DESC'],
+      limit:10
+    })
+      .then(function(result) {
+        return res.json(result);
+      });
   });
 
-  // POST route for saving a new todo. We can create a todo using the data on req.body
-  app.post("/api/todos", function(req, res) {
-    // orm.addTodo(req.body, function(results) {
-    //   res.json(results);
-    // });
+  // Search for Encounters by state & country
+
+  app.get("/api/:state?:country?", function(req,res) {
+
+    // Display data for all of the encounters.
+    Encounter.findAll({
+      where: {
+        state: req.params.state,
+        country: req.params.country
+      },
+      order: ['encounter_date','DESC'],
+      limit: 10
+    })
+      .then(function(result) {
+        return res.json(result);
+      });
   });
 
-  // DELETE route for deleting todos. We can access the ID of the todo to delete in
-  // req.params.id
-  app.delete("/api/todos/:id", function(req, res) {
-    // orm.deleteTodo(req.params.id, function(results) {
-    //   res.json(results);
-    // });
+  // Search for Encounters by country
+
+  app.get("/api/:country?", function(req,res) {
+
+    // Display data for all of the encounters.
+    Encounter.findAll({
+      where: {
+        country: req.params.country
+      },
+      order: ['encounter_date','DESC'],
+      limit: 10
+    })
+      .then(function(result) {
+        return res.json(result);
+      });
   });
 
-  // PUT route for updating todos. We can access the updated todo in req.body
-  app.put("/api/todos", function(req, res) {
-    // orm.editTodo(req.body, function(results) {
-    //   res.json(results);
-    // });
+  // Search for Encounters by date then provides JSON
+  app.get("/api/:date?", function(req,res) {
+
+    // Display data for all of the encounters.
+    Encounter.findAll({
+      where: {
+        encounter_date: req.params.date
+      }
+      order: ['encounter_date','DESC'],
+      limit: 10
+    })
+      .then(function(result){
+        return res.json(result);
+      });
+  });
+
+  // If a user sends data to add a new encounter...
+  app.post("/api/new", function(req, res) {
+
+    // Take the request...
+    var encounter = req.body;
+
+    // Then add the encounter to the database using sequelize
+    Encounter.create({
+      encounter_date: encounter.date,
+      city: encounter.city,
+      state: encounter.state,
+      country: encounter.country,
+      object_shape: encounter.shape,
+      duration: encounter.duration,
+      description: encounter.description,
+      reported_date: encounter.reported,
+      latitude: encounter.latitude,
+      longitude: encounter.longitude,
+      //zipcode: encounter.zipcode
+    });
+
   });
 };
