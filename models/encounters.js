e68343b4b428c58d2df04fa17aaa019939a246c5
Muriel <muriel.mangubat@gmail.com>
2018-01-12 12:05:22 -0600

    hard coded files

---
+++ models/encounters.js
@@ -0,0 +1,41 @@
+// Dependencies
+// =============================================================
+
+// Sequelize (capital) references the standard library
+var Sequelize = require("sequelize");
+// sequelize (lowercase) references our connection to the DB.
+var sequelize = require("../config/connection.js");
+
+// Creates a "Encounter" model that matches up with DB
+var Encounter = sequelize.define("encounter", {
+  // encounter date (date & time)
+  encounter_date: Sequelize.DATE,
+  // city of the encounter (a string)
+  city: Sequelize.STRING,
+  // state of the encounter (a string)
+  state: Sequelize.STRING,
+  // country of the encounter (a string)
+  country: Sequelize.STRING,
+  // shape of the encoutered object (a string)
+  object_shape: Sequelize.STRING,
+  // duration of the encounter (an int)
+  duration: Sequelize.INT,
+  // description of the encounter (a string)
+  description: Sequelize.STRING,
+  // date the encounter was reported (date & time)
+  reported_date: Sequelize.NOW,
+  // latitude coordinate of the encounter (decimal)
+  latitude: Sequelize.DECIMAL(25,20),
+  // longitude coordinate of the encounter (decimal)
+  longitude: Sequelize.DECIMAL(25,20),
+  // zipcode (a string)
+  zipcode: Sequelize.STRING
+}, {
+  timestamps: false
+});
+
+// Syncs with DB
+Encounter.sync();
+
+// Makes the Encounter Model available for other files (will also create a table)
+module.exports = Encounter;
--- server.js
+++ server.js
@@ -6,19 +6,30 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-var db = require("./models");
-
 
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Static directory to be served
+app.use(express.static("./public"));
+
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
+
+// Here we introduce HTML routing to serve different HTML files
+require("./routes/html-routes.js")(app);
 
 // Starts the server to begin listening
 // =============================================================
-db.sequelize.sync().then(function(){
-	app.listen(PORT, function() {
-  		console.log("App listening on PORT " + PORT);
-	});
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
 });
