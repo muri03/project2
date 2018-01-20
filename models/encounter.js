3de02357ae41588034a2b70d4c8ec1efdf4da392
vasmarm <sharmavashisth@gmail.com>
2018-01-20 14:51:23 -0600

    Freezing the table name

--- models/encounter.js
+++ models/encounter.js
@@ -13,7 +13,7 @@ var sequelize = require("../config/connection.js");
 
 // Creates a "Encounter" model that matches up with DB
 var Encounter = sequelize.define("alien_encounter", {
-    freezeTableName: true,
+     tableName: 'alien_encounter',
   // encounter date (date & time)
   encounter_date: Sequelize.DATE,
   // city of the encounter (a string)
